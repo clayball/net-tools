071ccaf40ac70b969e41bcaf47f208c87f7434d3
Clay <cwells73@gmail.com>
2016-02-03 17:16:19 -0500

    minor updates.

--- packet/syn-packet.py
+++ packet/syn-packet.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8 
 #
-# Clay Wells <cwells@cwells.org>
+# Clay Wells
 # 
 # syn-packet.py is based on a classic SYN scan. A single SYN packet is sent
 # to a single/multiple hosts and single/multiple ports and will quit after
-# receving a single response. 
+# receving a single response.
+#
+# MUST BE RUN USING SUDO --OR-- AS ROOT 
 #
 ###############################################################################
 # This program is free software: you can redistribute it and/or modify it     #
@@ -30,18 +32,19 @@ print '[+] SYN-Packet'
 print '[+]'
 
 # hosts to include in scan
-hosts = ["192.168.1.1","192.168.1.5","192.168.1.9"]
+hosts = ["204.79.197.200","173.194.43.96"]
 
 # set the ports to send a SYN packet (should makes sense to send it)
-ports = '22,80,443'
+#ports = '22,80,443'
 
 # send to single/multiple hosts/ports
-res,nores = sr(IP(dst=hosts)
-    /TCP(sport=RandShort(),dport=[22,80,123,443],flags="S"))
+res,nores = sr(IP(dst=hosts)/TCP(sport=RandShort(),dport=[80],flags="SA"))
 
 # =============================================================================
 # Results can be displayed in different ways.
 # =============================================================================
+
+# This needs some work! Why not use nmap istead?
 
 # only display packets with the “SA” flag set
 res.nsummary(lfilter = lambda (s,r): r.sprintf("%TCP.flags%") == "SA")
@@ -51,3 +54,6 @@ print '[+] =====================================================================
 # show what's open (single host)
 res.summary(lfilter = lambda (s,r): r.sprintf("%TCP.flags%") == "SA",
     prn=lambda(s,r):(s.dst, r.sprintf("%TCP.sport% is open")))
+
+print '[+] done'
+
